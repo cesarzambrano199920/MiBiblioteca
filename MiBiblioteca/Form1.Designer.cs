9a2f896770f49bf8bfd93b6f51a3197d04cb234f
cesarzambrano199920 <68898334+cesarzambrano199920@users.noreply.github.com>
2021-12-08 22:55:28 -0500

    Agregar archivos de proyecto.

---
+++ MiBiblioteca/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace MiBiblioteca
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.pnlPanelLateralDerecho = new System.Windows.Forms.Panel();
+            this.pnlPanelSuperiorDelpanelDerecho = new System.Windows.Forms.Panel();
+            this.pnlPanelDelAutor = new System.Windows.Forms.Panel();
+            this.pnlPanelCentral = new System.Windows.Forms.Panel();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlPanelLateralDerecho.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlPanelLateralDerecho
+            // 
+            this.pnlPanelLateralDerecho.BackColor = System.Drawing.Color.DimGray;
+            this.pnlPanelLateralDerecho.Controls.Add(this.panel1);
+            this.pnlPanelLateralDerecho.Controls.Add(this.button3);
+            this.pnlPanelLateralDerecho.Controls.Add(this.button2);
+            this.pnlPanelLateralDerecho.Controls.Add(this.btnAgregar);
+            this.pnlPanelLateralDerecho.Controls.Add(this.pnlPanelSuperiorDelpanelDerecho);
+            this.pnlPanelLateralDerecho.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlPanelLateralDerecho.Location = new System.Drawing.Point(0, 0);
+            this.pnlPanelLateralDerecho.Name = "pnlPanelLateralDerecho";
+            this.pnlPanelLateralDerecho.Size = new System.Drawing.Size(330, 749);
+            this.pnlPanelLateralDerecho.TabIndex = 0;
+            // 
+            // pnlPanelSuperiorDelpanelDerecho
+            // 
+            this.pnlPanelSuperiorDelpanelDerecho.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pnlPanelSuperiorDelpanelDerecho.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlPanelSuperiorDelpanelDerecho.BackgroundImage")));
+            this.pnlPanelSuperiorDelpanelDerecho.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlPanelSuperiorDelpanelDerecho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlPanelSuperiorDelpanelDerecho.Location = new System.Drawing.Point(0, 0);
+            this.pnlPanelSuperiorDelpanelDerecho.Name = "pnlPanelSuperiorDelpanelDerecho";
+            this.pnlPanelSuperiorDelpanelDerecho.Size = new System.Drawing.Size(330, 147);
+            this.pnlPanelSuperiorDelpanelDerecho.TabIndex = 0;
+            this.pnlPanelSuperiorDelpanelDerecho.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlPanelSuperiorDelpanelDerecho_Paint);
+            // 
+            // pnlPanelDelAutor
+            // 
+            this.pnlPanelDelAutor.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pnlPanelDelAutor.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlPanelDelAutor.Location = new System.Drawing.Point(1332, 0);
+            this.pnlPanelDelAutor.Name = "pnlPanelDelAutor";
+            this.pnlPanelDelAutor.Size = new System.Drawing.Size(346, 749);
+            this.pnlPanelDelAutor.TabIndex = 1;
+            // 
+            // pnlPanelCentral
+            // 
+            this.pnlPanelCentral.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pnlPanelCentral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPanelCentral.Location = new System.Drawing.Point(330, 0);
+            this.pnlPanelCentral.Name = "pnlPanelCentral";
+            this.pnlPanelCentral.Size = new System.Drawing.Size(1002, 749);
+            this.pnlPanelCentral.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.White;
+            this.btnAgregar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAgregar.BackgroundImage")));
+            this.btnAgregar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAgregar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(0, 147);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(330, 65);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(0, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(330, 65);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(0, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(330, 65);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 342);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(330, 230);
+            this.panel1.TabIndex = 4;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1678, 749);
+            this.Controls.Add(this.pnlPanelCentral);
+            this.Controls.Add(this.pnlPanelDelAutor);
+            this.Controls.Add(this.pnlPanelLateralDerecho);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mi biblioteca";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlPanelLateralDerecho.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPanelLateralDerecho;
+        private System.Windows.Forms.Panel pnlPanelSuperiorDelpanelDerecho;
+        private System.Windows.Forms.Panel pnlPanelDelAutor;
+        private System.Windows.Forms.Panel pnlPanelCentral;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
+
---
+++ MiBiblioteca/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiBiblioteca
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+            AbrirFormEnPanel(new ImagenCentral());
+            pnlPanelDelAutor.Visible = false;
+        }
+
+        //Abre los formularios en la pantalla principal
+        public void AbrirFormEnPanel(object Formhija)
+        {
+            if (this.pnlPanelCentral.Controls.Count > 0)
+                this.pnlPanelCentral.Controls.RemoveAt(0);
+            Form fh = Formhija as Form;
+            fh.TopLevel = false;
+            fh.Dock = DockStyle.Fill;
+            this.pnlPanelCentral.Controls.Add(fh);
+            this.pnlPanelCentral.Tag = fh;
+            fh.Show();
+        }
+
+        private void pnlPanelSuperiorDelpanelDerecho_Paint(object sender, PaintEventArgs e)
+        {
+            AbrirFormEnPanel(new ImagenCentral());
+            pnlPanelDelAutor.Visible = true;
+        }
+    }
+}
